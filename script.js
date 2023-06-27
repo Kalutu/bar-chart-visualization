e971c42e577fc43c586ba33713ace31f51e626f8
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-06-27 17:28:06 +0300

    modified:   script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log(d3)
+let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
+let req = new XMLHttpRequest();
+
+let data;
+let values;
+
+let heightScale;
+let xScale;
+let xAxisScale;
+let yAxisScale;
+
+let width =800;
+let height = 600;
+let padding=40;
+
+let svg = d3.select("svg");
+
+let drawCanvas = () => {
+    svg.attr("width",width)
+    svg.attr("height",height)
+}
+
+let generateScales = ()=>{
+
+}
+
+let drawBars = ()=>{
+
+}
+
+let generateAxes = ()=>{
+    
+}
